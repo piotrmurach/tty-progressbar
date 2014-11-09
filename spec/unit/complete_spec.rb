497041fb8e20346edadf239a079f9b35105f04b6
Piotr Murach <pmurach@gmail.com>
2014-11-09 20:45:03 +0000

    Fix typo.

--- spec/unit/complete_spec.rb
+++ spec/unit/complete_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe TTY::ProgressBar, '.complete?' do
   let(:output) { StringIO.new('', 'w+') }
 
   it "checks for completness" do
-    progress = TTY::ProgressBar.new("[;bar]", output: output, total: 3)
+    progress = TTY::ProgressBar.new("[:bar]", output: output, total: 3)
     completes = []
     3.times do
       completes << progress.complete?
