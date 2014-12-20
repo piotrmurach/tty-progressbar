af9837778519b0ac7b1ab9874fe3ab9a82f6031d
Piotr Murach <pmurach@gmail.com>
2014-12-20 21:37:18 +0100

    Test resizing.

---
+++ spec/unit/resize_spec.rb
@@ -0,0 +1,39 @@
+# coding: utf-8
+
+require 'spec_helper'
+
+RSpec.describe TTY::ProgressBar, '.resize' do
+  let(:output) { StringIO.new('', 'w+') }
+
+  it "resizes output down by x2" do
+    progress = TTY::ProgressBar.new("[:bar]", output: output, total: 5, width: 10)
+    2.times { progress.advance }
+    progress.resize(5)
+    3.times { progress.advance }
+    output.rewind
+    expect(output.read).to eq([
+      "\e[1G[==        ]",
+      "\e[1G[====      ]",
+      "\e[1G[==   ]",
+      "\e[1G[===  ]",
+      "\e[1G[==== ]",
+      "\e[1G[=====]\n"
+    ].join)
+  end
+
+  it "resizes output up by x2" do
+    progress = TTY::ProgressBar.new("[:bar]", output: output, total: 5, width: 10)
+    2.times { progress.advance }
+    progress.resize(20)
+    3.times { progress.advance }
+    output.rewind
+    expect(output.read).to eq([
+      "\e[1G[==        ]",
+      "\e[1G[====      ]",
+      "\e[1G[========            ]",
+      "\e[1G[============        ]",
+      "\e[1G[================    ]",
+      "\e[1G[====================]\n"
+    ].join)
+  end
+end
