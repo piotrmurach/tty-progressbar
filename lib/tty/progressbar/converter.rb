0444c9712a79ec32b56a5b546b9b1c0237b42d07
Piotr Murach <pmurach@gmail.com>
2014-12-20 17:34:17 +0100

    Add bytes converter.

--- lib/tty/progressbar/converter.rb
+++ lib/tty/progressbar/converter.rb
@@ -6,9 +6,17 @@ module TTY
     #
     # @api public
     class Converter
+      HOURSECONDS = 3600
+
+      # Convert seconds to time notation
+      #
+      # @param [Numeric] seconds
+      #   the seconds to convert to time
+      #
+      # @api public
       def to_time(seconds)
-        hours = (seconds / 3600.to_f).floor
-        seconds -= hours * 3600
+        hours = (seconds / HOURSECONDS.to_f).floor
+        seconds -= hours * HOURSECONDS
         minutes = (seconds / 60).floor
         seconds -= minutes * 60
 
@@ -20,6 +28,30 @@ module TTY
           sprintf('%2dm%2ds', minutes, seconds)
         else
           sprintf('%2ds', seconds)
+        end
+      end
+
+      KILOBYTE = 1024
+      MEGABYTE = KILOBYTE * 1024
+      GIGABYTE = MEGABYTE * 1024
+
+      # Convert value to bytes
+      #
+      # @param [Numeric] value
+      #   the value to convert to bytes
+      #
+      # @return [String]
+      #
+      # @api public
+      def to_bytes(value)
+        if value >= GIGABYTE
+          sprintf('%.2f', value / GIGABYTE.to_f) + 'GB'
+        elsif value >= MEGABYTE
+          sprintf('%.2f', value / MEGABYTE.to_f) + 'MB'
+        elsif value >= KILOBYTE
+          sprintf('%.2f', value / KILOBYTE.to_f) + 'KB'
+        else
+          value.to_s + 'B'
         end
       end
     end # Converter
---
+++ spec/unit/converter/to_bytes_spec.rb
@@ -0,0 +1,27 @@
+# coding: utf-8
+
+require 'spec_helper'
+
+RSpec.describe TTY::ProgressBar::Converter, '.to_bytes' do
+  subject(:converter) { described_class.new }
+
+  it "converts 1000 to bytes" do
+    expect(converter.to_bytes(1000)).to eq('1000B')
+  end
+
+  it "converts 1024 to bytes" do
+    expect(converter.to_bytes(1024)).to eq('1.00KB')
+  end
+
+  it "converts 2000 to bytes" do
+    expect(converter.to_bytes(2000)).to eq('1.95KB')
+  end
+
+  it "converts 10_000_000 to bytes" do
+    expect(converter.to_bytes(10_000_000)).to eq('9.54MB')
+  end
+
+  it "convert 10_000_000_000 to bytes" do
+    expect(converter.to_bytes(10_000_000_000)).to eq('9.31GB')
+  end
+end
