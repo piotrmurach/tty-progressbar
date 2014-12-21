b2a87510cf8170ad9368d192fc74b328776d47ea
Piotr Murach <pmurach@gmail.com>
2014-12-21 18:36:44 +0100

    Change precision.

--- spec/unit/formatter/byte_spec.rb
+++ spec/unit/formatter/byte_spec.rb
@@ -6,15 +6,15 @@ RSpec.describe TTY::ProgressBar, '.new' do
   let(:output) { StringIO.new('', 'w+') }
 
   it "displays bytes processed" do
-    progress = described_class.new(":byte", output: output, total: 100_000)
-    5.times { progress.advance(20_000) }
+    progress = described_class.new(":byte", output: output, total: 102_400)
+    5.times { progress.advance(20_480) }
     output.rewind
     expect(output.read).to eq([
-      "\e[1G19.53KB",
-      "\e[1G39.06KB",
-      "\e[1G58.59KB",
-      "\e[1G78.12KB",
-      "\e[1G97.66KB\n"
+      "\e[1G20.00KB",
+      "\e[1G40.00KB",
+      "\e[1G60.00KB",
+      "\e[1G80.00KB",
+      "\e[1G100.00KB\n"
     ].join)
   end
 end
