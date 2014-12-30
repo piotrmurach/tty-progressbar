331fcc3cba86e7e2ebf090450b146f92216bbfc7
Piotr Murach <pmurach@gmail.com>
2014-12-30 19:57:03 +0100

    Ensure correct elapsed time with reset.

--- spec/unit/formatter/elapsed_spec.rb
+++ spec/unit/formatter/elapsed_spec.rb
@@ -28,4 +28,36 @@ RSpec.describe TTY::ProgressBar, 'elapsed' do
     ].join)
     Timecop.return
   end
+
+  it "resets elapsed time" do
+    time_now = Time.local(2014, 10, 5, 12, 0, 0)
+    Timecop.freeze(time_now)
+    progress = TTY::ProgressBar.new(":elapsed", output: output, total: 5)
+
+    5.times do |sec|
+      time_now = Time.local(2014, 10, 5, 12, 0, sec)
+      Timecop.freeze(time_now)
+      progress.advance
+    end
+    expect(progress.complete?).to be(true)
+    progress.reset
+    2.times do |sec|
+      time_now = Time.local(2014, 10, 5, 13, 0, sec)
+      Timecop.freeze(time_now)
+      progress.advance
+    end
+
+    output.rewind
+    expect(output.read).to eq([
+      "\e[1G 0s",
+      "\e[1G 1s",
+      "\e[1G 2s",
+      "\e[1G 3s",
+      "\e[1G 4s\n",
+      "\e[1G 0s",
+      "\e[1G 0s",
+      "\e[1G 1s"
+    ].join)
+    Timecop.return
+  end
 end
