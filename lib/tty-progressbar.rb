ab8dc608d54e48b19801adf17bf0361cc8c7fb47
Piotr Murach <pmurach@gmail.com>
2014-12-31 20:49:20 +0100

    Change requires.

--- lib/tty-progressbar.rb
+++ lib/tty-progressbar.rb
@@ -1,3 +1,25 @@
 # coding: utf-8
 
+require 'io/console'
+require 'forwardable'
+require 'tty-screen'
+
+require 'tty/progressbar/configuration'
+require 'tty/progressbar/converter'
+require 'tty/progressbar/version'
+require 'tty/progressbar/pipeline'
+require 'tty/progressbar/formatter'
+require 'tty/progressbar/meter'
+
+require 'tty/progressbar/formatter/bar'
+require 'tty/progressbar/formatter/current'
+require 'tty/progressbar/formatter/current_byte'
+require 'tty/progressbar/formatter/elapsed'
+require 'tty/progressbar/formatter/estimated'
+require 'tty/progressbar/formatter/percent'
+require 'tty/progressbar/formatter/rate'
+require 'tty/progressbar/formatter/mean_rate'
+require 'tty/progressbar/formatter/total'
+require 'tty/progressbar/formatter/total_byte'
+
 require 'tty/progressbar'
