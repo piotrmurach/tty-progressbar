aa16aba688e42e636047ec1f3e9c7e53416f056c
Piotr Murach <pmurach@gmail.com>
2017-09-01 17:33:53 +0200

    Add example for multi threaded support in ref to issue #21

---
+++ examples/threaded.rb
@@ -0,0 +1,16 @@
+# encoding: utf-8
+
+require 'tty-progressbar'
+
+threads = []
+
+bar = TTY::ProgressBar.new("[:bar] :percent", total: 30)
+
+threads << Thread.new {
+  15.times { sleep(0.1); bar.update(complete: '-', head: '-'); bar.advance(); }
+}
+threads << Thread.new {
+  15.times { sleep(0.1); bar.update(complete: '+', head: '+'); bar.advance(); }
+}
+
+threads.map(&:join)
