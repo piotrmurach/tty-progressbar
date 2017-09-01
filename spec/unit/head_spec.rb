fdb6fbdd229beca32db1ab6b523fef17b65d7d14
Piotr Murach <pmurach@gmail.com>
2017-09-01 17:33:12 +0200

    Change spec to verify unicode support

--- spec/unit/head_spec.rb
+++ spec/unit/head_spec.rb
@@ -17,15 +17,18 @@ RSpec.describe TTY::ProgressBar, ':head' do
   end
 
   it "customises all output characters" do
-    progress = TTY::ProgressBar.new("[:bar]", output: output, head: '>', complete: '+', incomplete: '.', total: 5)
+    progress = TTY::ProgressBar.new("[:bar]",
+      output: output,
+      head: 'ᗧ',
+      complete: '-', incomplete: '.', total: 5)
     5.times { progress.advance }
     output.rewind
     expect(output.read).to eq([
-      "\e[1G[>....]",
-      "\e[1G[+>...]",
-      "\e[1G[++>..]",
-      "\e[1G[+++>.]",
-      "\e[1G[++++>]\n"
+      "\e[1G[ᗧ....]",
+      "\e[1G[-ᗧ...]",
+      "\e[1G[--ᗧ..]",
+      "\e[1G[---ᗧ.]",
+      "\e[1G[----ᗧ]\n"
     ].join)
   end
 end
