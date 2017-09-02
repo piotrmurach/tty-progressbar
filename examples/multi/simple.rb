400aef7a98627a23fde2f0a52dbc0e7db1d1b459
Piotr Murach <pmurach@gmail.com>
2017-09-02 21:05:20 +0200

    Update example display

--- examples/multi/simple.rb
+++ examples/multi/simple.rb
@@ -4,9 +4,9 @@ require 'tty-progressbar'
 
 bars = TTY::ProgressBar::Multi.new
 
-bar1 = bars.register "[:bar] one", total: 20
-bar2 = bars.register "[:bar] two", total: 30
-bar3 = bars.register "[:bar] three", total: 10
+bar1 = bars.register "foo [:bar] :percent", total: 20
+bar2 = bars.register "bar [:bar] :percent", total: 30
+bar3 = bars.register "baz [:bar] :percent", total: 10
 
 th1 = Thread.new { 20.times { sleep(0.2); bar1.advance } }
 th2 = Thread.new { 30.times { sleep(0.1); bar2.advance } }
