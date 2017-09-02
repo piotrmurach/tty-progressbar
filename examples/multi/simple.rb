c3faf58938f464c518b12972bcb6f434494194ad
Piotr Murach <pmurach@gmail.com>
2017-09-02 19:27:20 +0200

    Add example of multi bars setup in ref to issue #1

---
+++ examples/multi/simple.rb
@@ -0,0 +1,15 @@
+# encoding: utf-8
+
+require 'tty-progressbar'
+
+bars = TTY::ProgressBar::Multi.new
+
+bar1 = bars.register "[:bar] one", total: 20
+bar2 = bars.register "[:bar] two", total: 30
+bar3 = bars.register "[:bar] three", total: 10
+
+th1 = Thread.new { 20.times { sleep(0.2); bar1.advance } }
+th2 = Thread.new { 30.times { sleep(0.1); bar2.advance } }
+th3 = Thread.new { 10.times { sleep(0.3); bar3.advance } }
+
+[th1, th2, th3].each(&:join)
