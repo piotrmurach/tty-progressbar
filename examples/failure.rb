c65d828acde0b39df5e95ac2c217f12993652896
Piotr Murach <pmurach@gmail.com>
2017-09-02 14:38:07 +0200

    Add example of progress bar failure

---
+++ examples/failure.rb
@@ -0,0 +1,14 @@
+# encoding: utf-8
+
+require 'tty-progressbar'
+
+bar = TTY::ProgressBar.new("downloading [:bar] :percent", head: '>', total: 30)
+30.times do |i|
+  if i == 15
+    bar.update(head: 'x')
+    bar.stop
+    break
+  end
+  sleep(0.1)
+  bar.advance
+end
