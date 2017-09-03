dc5ee5e5e005c7b88e6f9f18de4ab166bcc98d4b
Piotr Murach <pmurach@gmail.com>
2017-09-03 19:10:03 +0200

    Add case for progress event

--- spec/unit/events_spec.rb
+++ spec/unit/events_spec.rb
@@ -3,7 +3,17 @@
 RSpec.describe TTY::ProgressBar, 'events' do
   let(:output) { StringIO.new('', 'w+') }
 
-  it "emits :done event" do
+  it "emits :progress event when advancing" do
+    events = []
+    bar = TTY::ProgressBar.new("[:bar]", output: output, total: 5)
+    bar.on(:progress) { events << :progress }
+
+    bar.advance
+
+    expect(events).to eq([:progress])
+  end
+
+  it "emits :done event when finished" do
     events = []
     bar = TTY::ProgressBar.new("[:bar]", output: output, total: 5)
     bar.on(:done) { events << :done }
