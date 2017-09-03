7655e4d65aa7af4a55f55da838a3adac5b04b060
Piotr Murach <pmurach@gmail.com>
2017-09-03 18:07:54 +0200

    Add unit test for multi bar stop behaviour

---
+++ spec/unit/multi/stop_spec.rb
@@ -0,0 +1,17 @@
+# encoding: utf-8
+
+RSpec.describe TTY::ProgressBar::Multi, '#stop' do
+  let(:output) { StringIO.new('', 'w+') }
+
+  it "stops all bars when top level stops" do
+    bars = TTY::ProgressBar::Multi.new("main [:bar]", output: output)
+
+    bar1 = bars.register("[:bar]", total: 5)
+    bar2 = bars.register("[:bar]", total: 10)
+
+    bars.stop
+
+    expect(bar1.stopped?).to eq(true)
+    expect(bar2.stopped?).to eq(true)
+  end
+end
