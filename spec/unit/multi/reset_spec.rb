e021e87c89fc7ae9865ce4934b74b8cadf0d30b9
Piotr Murach <pmurach@gmail.com>
2018-06-14 20:08:14 +0200

    Change to move multi bar reset test

---
+++ spec/unit/multi/reset_spec.rb
@@ -0,0 +1,28 @@
+RSpec.describe TTY::ProgressBar::Multi, '#reset' do
+  let(:output) { StringIO.new('', 'w+') }
+
+  it "leaves multibar state alone" do
+    main = TTY::ProgressBar::Multi.new("", output: output, total: 10)
+    progress = main.register("[:bar]")
+    progress.advance(10)
+    expect(progress.complete?).to be(true)
+    progress.reset
+    expect(progress.complete?).to be(false)
+    progress.advance(10)
+    output.rewind
+
+    top    = TTY::ProgressBar::Multi::DEFAULT_INSET[:top]
+    bottom = TTY::ProgressBar::Multi::DEFAULT_INSET[:bottom]
+
+    progress_updates =
+      output.read.scan(/#{Regexp.escape top}|#{Regexp.escape bottom}/)
+    expect(progress_updates.shift).to match(top)
+    expect(progress_updates.shift).to match(bottom)
+    expect(progress_updates.shift).to match(bottom)
+    expect(progress_updates.shift).to match(top)
+    expect(progress_updates.shift).to match(bottom)
+    expect(progress_updates.shift).to match(bottom)
+
+    expect(progress_updates).to be_empty
+  end
+end
--- spec/unit/reset_spec.rb
+++ spec/unit/reset_spec.rb
@@ -28,29 +28,4 @@ RSpec.describe TTY::ProgressBar, '#reset' do
       "\e[1G[======    ]"
     ].join)
   end
-
-  it "leaves multibar state alone" do
-    main = TTY::ProgressBar::Multi.new("", output: output, total: 10)
-    progress = main.register("[:bar]")
-    progress.advance(10)
-    expect(progress.complete?).to be(true)
-    progress.reset
-    expect(progress.complete?).to be(false)
-    progress.advance(10)
-    output.rewind
-
-    top    = TTY::ProgressBar::Multi::DEFAULT_INSET[:top]
-    bottom = TTY::ProgressBar::Multi::DEFAULT_INSET[:bottom]
-
-    progress_updates =
-      output.read.scan(/#{Regexp.escape top}|#{Regexp.escape bottom}/)
-    expect(progress_updates.shift).to match(top)
-    expect(progress_updates.shift).to match(bottom)
-    expect(progress_updates.shift).to match(bottom)
-    expect(progress_updates.shift).to match(top)
-    expect(progress_updates.shift).to match(bottom)
-    expect(progress_updates.shift).to match(bottom)
-
-    expect(progress_updates).to be_empty
-  end
 end
