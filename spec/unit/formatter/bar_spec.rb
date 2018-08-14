7f296ddc5fef3b442076738c744f735362813efb
Jonathon Sheffield <samsmug@gmail.com>
2018-08-14 14:24:19 -0700

    fix progress bar erroring when the amount of completed work is 0

--- spec/unit/formatter/bar_spec.rb
+++ spec/unit/formatter/bar_spec.rb
@@ -1,3 +1,4 @@
+require 'pastel'
 RSpec.describe TTY::ProgressBar, ':bar token' do
   let(:output) { StringIO.new('', 'w+') }
 
@@ -12,5 +13,15 @@ RSpec.describe TTY::ProgressBar, ':bar token' do
       "\e[1G[==== ]",
       "\e[1G[=====]\n"
     ].join)
+  end
+
+  it "properly handles the case of a large amount of work on a small bar that has colors for complete" do
+    pastel = Pastel.new
+    progress = TTY::ProgressBar.new("[:bar]",
+      total: 1000,
+      complete: pastel.on_green(' '),
+      width: 80
+    )
+    expect{ 5.times { progress.advance(20) } }.to_not raise_error
   end
 end
--- tty-progressbar.gemspec
+++ tty-progressbar.gemspec
@@ -27,4 +27,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'rspec', '~> 3.1'
   spec.add_development_dependency 'timecop', '~> 0.9.1'
   spec.add_development_dependency 'rake'
+  spec.add_development_dependency 'pastel'
 end
