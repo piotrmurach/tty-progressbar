4155814cfb9c67b87d9a2ddf1b57b6ab22e4068a
Piotr Murach <pmurach@gmail.com>
2020-07-07 23:52:31 +0100

    Change example to demonstrate unicode support for incomplete characters

--- examples/unicode.rb
+++ examples/unicode.rb
@@ -1,6 +1,7 @@
-require_relative '../lib/tty-progressbar'
+require_relative "../lib/tty-progressbar"
 
-bar = TTY::ProgressBar.new("Unicode [:bar]", total: 30, complete: 'あ')
+bar = TTY::ProgressBar.new("Unicode [:bar]", total: 30,
+                           head: ">", complete: "本", incomplete: "〜")
 30.times do
   sleep(0.1)
   bar.advance
