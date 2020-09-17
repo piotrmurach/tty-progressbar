d2f65c995ee43c265c694de33d8ce5cc5c0e47cf
Piotr Murach <pmurach@gmail.com>
2020-09-17 13:56:43 +0100

    Add indeterminate progress bar example

---
+++ examples/indeterminate.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require_relative "../lib/tty-progressbar"
+
+bar = TTY::ProgressBar.new("downloading [:bar] :current/:total :current_byte " \
+                           ":total_byte :percent ET::elapsed ETA::eta " \
+                           ":rate/s :mean_rate/s :byte_rate/s :mean_byte/s",
+                           width: 40)
+
+170.times do |i|
+  sleep(0.05)
+  bar.advance
+  if i == 69
+    bar.update(indeterminate: false, total: 170)
+  end
+end
