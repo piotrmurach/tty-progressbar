dd8fe57ce782a90d47e8166145d75a9050ff57dd
Piotr Murach <pmurach@gmail.com>
2021-01-04 21:53:39 +0100

    Change to ensure unknown progress is configured for all bar formats

--- spec/unit/bar_format_spec.rb
+++ spec/unit/bar_format_spec.rb
@@ -10,7 +10,9 @@ RSpec.describe TTY::ProgressBar, ":bar_format" do
       incomplete = formats[format][:incomplete]
       progress = TTY::ProgressBar.new("[:bar]", output: output, total: 10,
                                                 bar_format: format)
+
       5.times { progress.advance(2) }
+
       output.rewind
       expect(output.read).to eq([
         "\e[1G[#{complete * 2}#{incomplete * 8}]",
@@ -18,6 +20,21 @@ RSpec.describe TTY::ProgressBar, ":bar_format" do
         "\e[1G[#{complete * 6}#{incomplete * 4}]",
         "\e[1G[#{complete * 8}#{incomplete * 2}]",
         "\e[1G[#{complete * 10}#{incomplete * 0}]\n"
+      ].join)
+    end
+
+    it "displays unknown progress for #{format.inspect} bar format" do
+      unknown = formats[format][:unknown]
+      left_chars = 10 - unknown.size
+      progress = TTY::ProgressBar.new("[:bar]", output: output, total: nil,
+                                                bar_format: format, width: 10)
+
+      2.times { progress.advance(2) }
+
+      output.rewind
+      expect(output.read).to eq([
+        "\e[1G[#{unknown}#{" " * left_chars}]",
+        "\e[1G[#{unknown}#{" " * left_chars}]",
       ].join)
     end
   end
