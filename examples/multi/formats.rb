499e3531b763b83d956fa488ec3dd023fea134bd
Piotr Murach <pmurach@gmail.com>
2021-01-06 22:48:47 +0100

    Change example to fit new bar formats

--- examples/multi/formats.rb
+++ examples/multi/formats.rb
@@ -6,13 +6,13 @@ bars = []
 multi_bar = TTY::ProgressBar::Multi.new(width: 50)
 
 TTY::ProgressBar::Formats::FORMATS.each_key do |format|
-  bars << multi_bar.register("%8s |:bar|" % [format],
+  bars << multi_bar.register("%12s |:bar|" % [format],
                              total: 50, bar_format: format)
 end
 
 50.times do
   bars.each do |bar|
-    sleep(0.01)
+    sleep(0.002)
     bar.advance
   end
 end
