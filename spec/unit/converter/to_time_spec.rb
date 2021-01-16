31c1428badf614fd7e200b55623deb206b19900b
Piotr Murach <pmurach@gmail.com>
2021-01-16 13:16:44 +0100

    Add tests to ensure time conversion

--- spec/unit/converter/to_time_spec.rb
+++ spec/unit/converter/to_time_spec.rb
@@ -3,16 +3,36 @@
 RSpec.describe TTY::ProgressBar::Converter, "#to_time" do
   subject(:converter) { described_class }
 
+  it "converts zero seconds" do
+    expect(converter.to_time(0.0)).to eq(" 0s")
+  end
+
+  it "converts less than a second to zero" do
+    expect(converter.to_time(0.83)).to eq(" 0s")
+  end
+
+  it "converts seconds to a second" do
+    expect(converter.to_time(1.2)).to eq(" 1s")
+  end
+
   it "converts seconds to seconds" do
     expect(converter.to_time(15)).to eq("15s")
   end
 
-  it "converts seconds to minutes" do
+  it "converts seconds to a minute" do
     expect(converter.to_time(100)).to eq(" 1m40s")
   end
 
-  it "converts seconds to small hours" do
+  it "converts seconds to minutes" do
+    expect(converter.to_time(2000)).to eq("33m20s")
+  end
+
+  it "converts seconds to an hour" do
     expect(converter.to_time(3600)).to eq(" 1h 0m")
+  end
+
+  it "converts seconds to hours" do
+    expect(converter.to_time(23.5 * 3600)).to eq("23h30m")
   end
 
   it "converts seconds to days and hours" do
