75849ff987d603928e35c913882dc7a7b679e57f
Alex Watt <alex@alexcwatt.com>
2023-08-23 05:58:19 -0400

    Fix setting the current progress for an indeterminate bar

--- spec/unit/set_current_spec.rb
+++ spec/unit/set_current_spec.rb
@@ -33,6 +33,17 @@ RSpec.describe TTY::ProgressBar, "#current=" do
     expect(progress.current).to eq(0)
   end
 
+  it "doesn't allow nil" do
+    progress = TTY::ProgressBar.new("[:bar]", output: output, total: 10)
+    expect {
+      progress.current = nil
+    }.to raise_error(
+      ArgumentError,
+      "Expected a numeric value, got nil instead."
+    )
+    expect(progress.current).to eq(0)
+  end
+
   it "cannot backtrack on finished" do
     progress = TTY::ProgressBar.new("[:bar]", output: output, total: 10)
     progress.current = 10
@@ -41,5 +52,11 @@ RSpec.describe TTY::ProgressBar, "#current=" do
     expect(progress.current).to eq(10)
     output.rewind
     expect(output.read).to eq("\e[1G[==========]\n")
+  end
+
+  it "allows setting progress when the total is unknown" do
+    progress = TTY::ProgressBar.new("[:bar]", output: output, total: nil)
+    progress.current = 5
+    expect(progress.current).to eq(5)
   end
 end
