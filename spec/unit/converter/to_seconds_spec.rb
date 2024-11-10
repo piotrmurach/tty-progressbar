58a66a6ca74b25322e5957b334b796b1970302e6
Piotr Murach <pmurach@gmail.com>
2024-11-10 13:59:42 +0100

    Fix conversion to seconds test to work on TruffleRuby

--- spec/unit/converter/to_seconds_spec.rb
+++ spec/unit/converter/to_seconds_spec.rb
@@ -3,15 +3,21 @@
 RSpec.describe TTY::ProgressBar::Converter, "#to_seconds" do
   subject(:converter) { described_class }
 
-  it "ensure 5 digit precision for < 1" do
+  it "ensures 5-digit precision for < 1",
+     unless: RSpec::Support::Ruby.truffleruby? do
     expect(converter.to_seconds(0.000005)).to eq("0.00001")
+  end
+
+  it "ensures 5-digit precision for < 1 on TruffleRuby",
+     if: RSpec::Support::Ruby.truffleruby? do
+    expect(converter.to_seconds(0.000005)).to eq("0.00000")
   end
 
   it "rounds 0 to 0.00" do
     expect(converter.to_seconds(0)).to eq(" 0.00")
   end
 
-  it "ensures 2 digit precision for > 1" do
+  it "ensures 2-digit precision for > 1" do
     expect(converter.to_seconds(11.2)).to eq("11.20")
   end
 
